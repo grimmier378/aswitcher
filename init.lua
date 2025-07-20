15429120d0af334da2ed6ec818647ef69aca416e
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2025-07-20 14:23:13 -0600

    cleanup: cleaned up unneeded code

--- init.lua
+++ init.lua
@@ -1,62 +1,17 @@
-local mq         = require('mq')
-local ImGui      = require('ImGui')
-local Actors     = require('actors')
-local Icons      = require('mq.ICONS')
+local mq        = require('mq')
+local Actors    = require('actors')
 
-local MySelf     = mq.TLO.Me
-local MyName     = MySelf.CleanName()
-local MyClass    = (MySelf.Class.ShortName() or 'unknown'):lower()
-local MyActor    = nil
+local MySelf    = mq.TLO.Me
+local MyName    = MySelf.CleanName()
+local MyActor   = nil
 
-local Boxes      = {}  -- holds actors data
-Boxes[MyName]    = nil -- holds my data
-local ActorsList = {}  -- holds the list of actors
-local lastCheck  = 0
-
-local isRunning  = true
-local ShowMain   = false
-
---- Sort the actors list alphabetically.
-local function SortActors()
-	ActorsList = {}
-	for actorName, actorData in pairs(Boxes) do
-		table.insert(ActorsList, actorName)
-	end
-	table.sort(ActorsList, function(a, b)
-		return a < b
-	end)
-end
+local isRunning = true
 
 -- ACTORS --
-
-local function CheckIn()
-	if MyActor then
-		MyActor:send({ mailbox = 'actorswitcher', }, {
-			Subject = 'Hello',
-			From = MyName,
-			Class = MyClass,
-		})
-	end
-	lastCheck = os.time()
-end
 
 local function ActorsHandler()
 	MyActor = Actors.register('actorswitcher', function(message)
 		local newMessage = message()
-
-		if newMessage.Subject == 'Hello' and newMessage.From ~= MyName then
-			if Boxes[newMessage.From] == nil then
-				Boxes[newMessage.From] = newMessage.Class
-				if MyActor then
-					MyActor:send({ mailbox = 'actorswitcher', }, {
-						Subject = 'Hello',
-						From = MyName,
-						Class = MyClass,
-					})
-				end
-				return
-			end
-		end
 
 		if newMessage.Subject == 'switch' and newMessage.Who:lower() == MyName:lower() then
 			mq.cmd("/foreground")
@@ -69,30 +24,16 @@ end
 local function CommandHandler(...)
 	local args = { ..., }
 
-	if args[1] == 'list' then
-		for _, actorName in pairs(ActorsList) do
-			if actorName ~= MyName then
-				printf("%s (%s)", actorName, Boxes[actorName])
-			end
-		end
-	elseif args[1] == 'hello' then
-		printf("Saying Hello")
-		if MyActor then
-			MyActor:send({ mailbox = 'actorswitcher', }, {
-				Subject = 'Hello',
-				From = MyName,
-				Class = MyClass,
-			})
-			return
-		else
-			mq.cmdf("/echo No actor registered for hello.")
-		end
-	elseif args[1] ~= nil then
+	if args[1] == 'quit' or args[1] == 'exit' then
+		printf("Exiting ActorsSwitcher...")
+		isRunning = false
+		return
+	end
+	if args[1] ~= nil then
 		if MyActor then
 			MyActor:send({ mailbox = 'actorswitcher', }, {
 				Subject = 'switch',
 				From = MyName,
-				Class = MyClass,
 				Who = args[1],
 			})
 			return
@@ -109,27 +50,16 @@ local function Init()
 	-- initialize Actors
 	ActorsHandler()
 	mq.bind("/aswitch", CommandHandler)
-	if MyActor then
-		MyActor:send({ mailbox = 'actorswitcher', }, {
-			Subject = 'Hello',
-			From = MyName,
-			Class = MyClass,
-		})
-	end
-	printf("ActorsSwitcher initialized for %s (%s)", MyName, MyClass)
+
+	printf("ActorsSwitcher initialized for %s", MyName)
 	printf("Use /aswitch name to switch to another actor.")
-	printf("Use /aswitch list to show the list of actors.")
-	printf("Use /aswitch hello announce yourself to others (updates list).")
+	printf("Use /aswitch quit to exit the Script.")
 	isRunning = true
 end
 
 local function Main()
 	while isRunning do
 		mq.delay(100)
-		SortActors()
-		if os.time() - lastCheck > 10 then
-			CheckIn()
-		end
 	end
 	mq.unbind("/aswitch")
 end
